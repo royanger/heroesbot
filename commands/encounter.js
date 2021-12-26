33a9713f9c9e908a66941184c355795c7fcdb072
Roy Anger <roy@royanger.com>
2021-12-25 19:24:32 -0500

    Added support to iterate over everything in /teach directory and require all files, and add each encounter as a choice.

--- commands/encounter.js
+++ commands/encounter.js
@@ -1,43 +1,44 @@
 const fs = require('fs');
+const path = require('path');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { guildId, botName } = require('../configs/bot.config.json');
 const logger = require('../libs/logger');
 
+fs.readdirSync('./teach').forEach((dir) => {
+  fs.readdirSync(`./teach/${dir}`).forEach((file) => {
+    let fileName = file.split('.').slice(0, 1).join('');
+    module.exports[fileName] = require(path.join(
+      __dirname,
+      `../teach/${dir}`,
+      file
+    ));
+  });
+});
+
 let lessons = [];
 
 // create object of raids/events with array of encounters based on file system
-fs.readdirSync('./teach')
-  //   .filter((file) => file.endsWith('.js'));
-  .forEach((dir) => {
-    let encounters = [];
-    fs.readdirSync(`./teach/${dir}`).forEach((file) => {
-      encounters.push(file.split('.').slice(0, 1).join(''));
-    });
-    lessons[dir] = encounters;
+fs.readdirSync('./teach').forEach((dir) => {
+  let encounters = [];
+  fs.readdirSync(`./teach/${dir}`).forEach((file) => {
+    encounters.push(file.split('.').slice(0, 1).join(''));
   });
-console.log('=======');
-console.log(lessons);
+  lessons[dir] = encounters;
+});
 
-// add /hero command
+// add /encounter command
 let data = new SlashCommandBuilder()
   .setName('encounter')
   .setDescription(`Provide information about encounter`);
 
 // iterate over the various event types and add subcommands for each
-
-data.addStringOption((option) =>
-  option
-    .setName('event')
-    .setDescription('test1')
-    .addChoice('Wish Wall', 'wish-wall')
-    .addChoice('Kalli', 'kalli')
-);
-
-console.log('=======');
-Object.keys(lessons).map((raid, index) => {
-  console.log(raid);
-  lessons[raid].map((encounter) => {
-    console.log(encounter);
+Object.keys(lessons).map((raid) => {
+  data.addStringOption((option) => {
+    option.setName(raid).setDescription(`Encounters for ${raid}`);
+    lessons[raid].map((encounter) => {
+      option.addChoice(module.exports[encounter].name, encounter);
+    });
+    return option;
   });
 });
 
