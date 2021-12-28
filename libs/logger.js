7e7a6e343df6c19a8e12be1d02783b500868022b
Roy Anger <roy@royanger.com>
2021-12-27 19:09:42 -0500

    Added package and config for log rotation

--- libs/logger.js
+++ libs/logger.js
@@ -1,9 +1,22 @@
 const bunyan = require('bunyan');
 const { logPath, botLoggerName } = require('../configs/bot.config.json');
+const RotatingFileStream = require('bunyan-rotating-file-stream');
 
 const log = bunyan.createLogger({
   name: botLoggerName,
-  streams: [{ path: logPath }],
+  streams: [
+    {
+      stream: new RotatingFileStream({
+        path: logPath,
+        period: '1d', // daily rotation
+        totalFiles: 10, // keep up to 10 back copies
+        rotateExisting: true, // Give ourselves a clean file when we start up, based on period
+        threshold: '10m', // Rotate log files larger than 10 megabytes
+        totalSize: '20m', // Don't keep more than 20mb of archived log files
+        gzip: true, // Compress the archive log files to save space
+      }),
+    },
+  ],
 });
 
 exports.trace = (message) => {
